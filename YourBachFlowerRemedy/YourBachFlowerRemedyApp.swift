a2f350259a2bd4247dae1bceb8d8599627e3a323
ads63 <48177421+ads63@users.noreply.github.com>
2023-05-02 12:58:04 +0300

    Initial Commit

---
+++ YourBachFlowerRemedy/YourBachFlowerRemedyApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourBachFlowerRemedyApp.swift
+//  YourBachFlowerRemedy
+//
+//  Created by Алексей Шинкарев on 02.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct YourBachFlowerRemedyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
